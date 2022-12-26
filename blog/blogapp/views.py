4871fd4957416525099ed8627cf2e6840b1fcbe8
Pav9551 <pav9551@yandex.ru>
2022-12-26 18:00:25 +0300

    add index.html

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
+posts =Post.objects.all()
 # Create your views here.
 def main_view(request):
-    return render(request, 'blogapp/index.html', context = {})
+    return render(request, 'blogapp/index.html', context = {'posts', posts})
 
