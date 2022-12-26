e8bc4178bd02a95404206f871843478d2fef9c1a
Pav9551 <pav9551@yandex.ru>
2022-12-26 21:26:48 +0300

    post :

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -3,5 +3,5 @@ from .models import Post
 posts =Post.objects.all()
 # Create your views here.
 def main_view(request):
-    return render(request, 'blogapp/index.html', context = {'posts', posts})
+    return render(request, 'blogapp/index.html', context = {'posts': posts})
 
