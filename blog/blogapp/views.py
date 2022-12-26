1e26bb3b82d78c67d0c60adffcd060a10c402ef4
Pav9551 <pav9551@yandex.ru>
2022-12-26 22:33:24 +0300

    git ignore

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-posts =Post.objects.all()
+posts = Post.objects.all()
 # Create your views here.
 def main_view(request):
     return render(request, 'blogapp/index.html', context = {'posts': posts})
