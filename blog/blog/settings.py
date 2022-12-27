f788fe932f70c5a16b5648b4065781fd87a0921a
Pav9551 <pav9551@yandex.ru>
2022-12-27 17:03:38 +0300

    add image

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -123,3 +123,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/emails'
