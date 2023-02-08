19b324386b92233c604883939d2af582a87f1db6
Pav9551 <pav9551@yandex.ru>
2023-02-08 11:45:45 +0300

    added test

--- blog/blogapp/tests.py
+++ blog/blogapp/tests.py
@@ -4,13 +4,16 @@ from django.test import TestCase
 from .models import Post, Category
 from usersapp.models import BlogUser
 class PostTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name = 'test_category')
+        self.user = BlogUser.objects.create_user(username = 'test_user', email = 'Q@QQ.ru', password = 'Django123')
+        self.post = Post.objects.create(name = 'test_post', text = 'some', user = self.user, category = self.category)
+
     def test_has_image(self):
-        category = Category.objects.create(name = 'test_category')
-        user = BlogUser.objects.create_user(username = 'test_user', email = 'Q@QQ.ru', password = 'Django123')
-        post = Post.objects.create(name = 'test_post', text = 'some', user = user, category = category)
-        self.assertFalse(post.has_image())
+        self.assertFalse(self.post.has_image())
     def test_some_method(self):
-        category = Category.objects.create(name = 'test_category')
-        user = BlogUser.objects.create_user(username = 'test_user', email = 'Q@QQ.ru', password = 'Django123')
-        post = Post.objects.create(name = 'test_post', text = 'some', user = user, category = category)
-        self.assertFalse(post.some_method() == 'some method1')
+        post = Post.objects.get(name = 'test_post')
+        self.assertFalse(self.post.some_method() == 'some method1')
+
+    def test_str(self):
+        self.assertEqual(str(self.post_str), 'test_post_str, category: test_category')
